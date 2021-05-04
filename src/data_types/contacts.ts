79cab566a06fbe65829be9892f8d85f6903ffa61
van-damm <64220302+van-damm@users.noreply.github.com>
2021-05-04 16:37:32 +0100

    Create index.ts

---
+++ src/data_types/contacts.ts
@@ -0,0 +1,4 @@
+export type Contacts = {
+  listing_id: number,
+  contact_date: Date | number | string
+}
---
+++ src/data_types/index.ts
@@ -0,0 +1,5 @@
+export * from './contacts';
+export * from './listings';
+export * from './car_make';
+export * from './results';
+export * from './sellers';
