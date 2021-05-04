393ce184d4bf5ced6bbf2d88b5d1c9e11e9b7fdd
van-damm <64220302+van-damm@users.noreply.github.com>
2021-05-04 16:45:52 +0100

    Create index.ts

---
+++ src/functions/index.ts
@@ -0,0 +1,6 @@
+export * from './calculate_average';
+export * from './calculate_distribution';
+export * from './contacts';
+export * from './convert_currency';
+export * from './csv_files';
+export * from './listings';
